24d8ae9b34281194447a6894d3799ef5fc5cea9f
bcapuano <bcapuano@asu.edu>
2022-09-25 21:11:08 -0400

    Initial Mayhem Heroes Integration

---
+++ mayhem/fuzz_base58.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import atheris
+import sys
+
+with atheris.instrument_imports():
+    from base58 import b58encode, b58decode
+
+
+@atheris.instrument_func
+def TestOneInput(data):
+    encoded = b58encode(data)
+    decoded = b58decode(encoded)
+
+    if encoded != decoded:
+        raise RuntimeError("Encoded and decoded data are not equal")
+
+
+def main():
+    atheris.Setup(sys.argv, TestOneInput, enable_python_coverage=True)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
